7f14af48c6332519f7612c74c171e6ec416cbf8b
Maulon <tmaulon@betomorrow.com>
2020-01-02 20:46:35 +0100

    trying improve API requests

--- Services/API/RecipeSearch.tsx
+++ Services/API/RecipeSearch.tsx
@@ -7,21 +7,40 @@ export const APP_KEY = "";
  * Exemple  "https://api.edamam.com/search?q=chicken&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free";
  *
  */
-const RecipesSearchURLBase = "https://api.edamam.com/search";
+export const RecipesSearchURLBase = "https://api.edamam.com/search";
 
-export const getRecipesFromApiWithOnlySearchedText = async (text: string) => {
-  const url = `${RecipesSearchURLBase}?q=${text}&app_id=${APP_ID}&app_key=${APP_KEY}`;
+export const getRecipesCountFromApiWithSearchedTextOnly: (
+  query: string
+) => Promise<number> = async (query: string) => {
+  const url = `${RecipesSearchURLBase}?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`;
+  const response = await fetch(url);
+  const data = await response.json();
+  return data.count;
+};
+
+export const getRecipesFromApiWithSearchedTextOnly = async (query: string) => {
+  const url = `${RecipesSearchURLBase}?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`;
+  const response = await fetch(url);
+  const data = await response.json();
+  return data.hits;
+};
+export const getMoreRecipesFromApiWithSearchedTextOnly = async (
+  query: string,
+  minPageNumber: number,
+  lastPageNumber: number
+) => {
+  const url = `${RecipesSearchURLBase}?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&from=${minPageNumber}&to=${lastPageNumber}`;
   const response = await fetch(url);
   const data = await response.json();
   return data.hits;
 };
 
 export const getRecipesFromApiWithSearchedTextAndMaximumCalories = async (
-  text: string,
+  query: string,
   maximumCalories: number
 ) => {
   const url = `
-    ${RecipesSearchURLBase}?q=${text}&app_id=${APP_ID}&app_key=${APP_KEY}&calories=0-${maximumCalories}`;
+    ${RecipesSearchURLBase}?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&calories=0-${maximumCalories}`;
   const response = await fetch(url);
   const data = await response.json();
   console.log(data.hits);
